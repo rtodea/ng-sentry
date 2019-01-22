d4341e10f3f4d53cb109810cbbc1930e642c23e8
Robert Todea <robert.todea@e-spres-oh.com>
2019-01-22 14:23:30 +0200

    Add sentry.io to this project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-sentry';
+
+  throwError() {
+    console.log('AppComponent.throwError()');
+    const o = {};
+    o['this'].that();
+    console.log(o['this'].that());
+    throw new Error('AppComponent:throwError');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ErrorHandler, Injectable, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { BrowserOptions, init, captureException } from '@sentry/browser';
+
+const sentryOptions = {
+  dsn: 'https://9570f9c991ec4399b9927accfcef05e9@sentry.io/1365690',
+} as BrowserOptions;
+
+init(sentryOptions);
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+  handleError(error) {
+    console.log('SentryErrorHandler:handleError', error);
+    captureException(error.originalError || error);
+    throw error;
+  }
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +30,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
